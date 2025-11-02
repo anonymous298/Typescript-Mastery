360104729f8c37f5bb6e778aa9318dd826dac346
anonymous298 <tackletalha@gmail.com>
2025-11-02 15:29:25 +0500

    typescript type inference and type annotations covered

---
+++ Type-Inference-And-Annotations/app.js
@@ -0,0 +1,11 @@
+// Type Inference
+// let age: number = 18;
+var age = 'talhha';
+//Type Annotations
+var name = 'talhha';
+var arr = ['talha', 18];
+var union = true;
+function doSomething(a, b, c, d) {
+    var something = {};
+    return something;
+}
---
+++ Type-Inference-And-Annotations/app.ts
@@ -0,0 +1,18 @@
+// Type Inference
+
+// let age: number = 18;
+
+let age = 'talhha'
+
+//Type Annotations
+
+let name: string = 'talhha'
+
+let arr: [string, number] = ['talha', 18]
+
+let union: string | number | boolean = true
+
+function doSomething(a: string, b: number, c: boolean, d?:string): object {
+    const something: object = {}
+    return something
+}
