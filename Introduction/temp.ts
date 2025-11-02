f0a4bcbd1ba39fe2f25066ce9611f7667d52f3f7
anonymous298 <tackletalha@gmail.com>
2025-11-02 15:15:18 +0500

    typescript introduction completed

---
+++ Introduction/temp.ts
@@ -0,0 +1,23 @@
+// interface Car {
+//     model: string;
+//     type: string;
+//     manufactureDate: number;
+// }
+
+// const civic: Car = {
+//     model : 'civic 2000',
+//     type : 'petrol',
+//     manufactureDate : 2000
+// }
+
+// function handleDelete(id?: number): string {
+//     const name: string = 'talha';
+//     return name;
+// }
+
+// const age: number = 10
+
+// function getData(): object {
+//     return {data : 'data'}
+// }
+
