b9abc098a003963181332189a572efda758ed6bd
anonymous298 <tackletalha@gmail.com>
2025-11-05 12:05:48 +0500

    learned more about interfaces and new type aliases

--- Interfaces-And-Type-Aliases/app.ts
+++ Interfaces-And-Type-Aliases/app.ts
@@ -17,3 +17,48 @@ function doSomething(obj: User) {
 }
 
 doSomething({name : 'talhah', email : 'talha@gmail.com', password : 'abcd123'})
+
+
+// Extending Interfaces
+
+interface Engine {
+    engine: string,
+    engineModel: string,
+    engineManufactureDate: number
+}
+
+interface Car extends Engine {
+    carModel: string,
+    carCompany: string
+}
+
+function manufactureCar(details: Car) {
+    
+}
+
+manufactureCar({
+    engine : 'petrol',
+    engineModel : 'v8',
+    engineManufactureDate : 1997,
+    carModel : 'Land Cruiser',
+    carCompany : 'toyota'
+}) 
+
+
+// Type Aliases
+
+type custom = number | string | boolean
+
+let a: custom  = 'talha'
+
+type arg = number | boolean
+
+function abcd(val: arg) {
+
+}
+
+abcd(true)
+
+
+// Intersection types
+
